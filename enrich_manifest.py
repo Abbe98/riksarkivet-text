da4124d22d6b5f16c52fd82188da8a077bfc8329
Abbe98 <albin.post@gmail.com>
2023-12-05 21:17:00 +0100

    first commit

---
+++ enrich_manifest.py
@@ -0,0 +1,19 @@
+import json
+
+with open('original-manifest.json') as json_file:
+    original_mainfest_data = json.load(json_file)
+
+for canvas in original_mainfest_data['items']:
+    id = canvas['id']
+    # extract the numeric id from the uri (https://lbiiif.riksarkivet.se/arkis!30003111_00004/canvas -> 30003111_00004)
+    id = id.split('/')[-2]
+
+    canvas['seeAlso'] = {
+        '@id': f'https://byabbe.se/riksarkivet-text/alto/{id}.xml',
+        'profile': 'http://www.loc.gov/standards/alto/ns-v4#',
+        'format': 'application/xml'
+    }
+
+# write to new file
+with open('enriched_manifest.json', 'w') as outfile:
+    json.dump(original_mainfest_data, outfile, indent=4)
